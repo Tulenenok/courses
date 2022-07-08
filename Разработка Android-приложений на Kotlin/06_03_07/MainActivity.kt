13e683bc8da5dce5f03256ed83f9823925771e3f
Natasha Gurova <78147880+Tulenenok@users.noreply.github.com>
2022-07-08 16:42:55 +0300

    ---

---
+++ Разработка Android-приложений на Kotlin/06_03_07/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.company.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var dataArray = MutableList(31, { it -> it.toString()})
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataArray)
+        gridView.adapter = adapter
+
+        gridView.numColumns = 4
+
+        gridView.setOnItemClickListener{parent, view, position, id ->
+            dataArray.remove(dataArray[position])
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
